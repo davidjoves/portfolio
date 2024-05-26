1121d94485153d250380ba3d0930141f0c258541
davidjoves <davidjoves20@gmail.com>
2024-05-26 12:50:55 -0700

    -m

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 function copyEmailToClipboard() {
     const email = "djoves@uci.edu"; // Replace with your email address
-    navigator.clipboard.writeText(email).then(() => {
-        showCustomAlert('Email copied to clipboard!');
-    }).catch(err => {
-        console.error('Could not copy text: ', err);
-    });
+    navigator
+        .clipboard
+        .writeText(email)
+        .then(() => {
+            showCustomAlert('Email copied to clipboard!');
+        })
+        .catch(err => {
+            console.error('Could not copy text: ', err);
+        });
 }
 
 function showCustomAlert(message) {
     const alertBox = document.getElementById('customAlert');
     alertBox.textContent = message;
-    alertBox.classList.add('show');
+    alertBox
+        .classList
+        .add('show');
     setTimeout(() => {
-        alertBox.classList.remove('show');
+        alertBox
+            .classList
+            .remove('show');
     }, 3000); // The alert will be visible for 3 seconds
 }
 
-document.getElementById('copyEmailButton').addEventListener('click', function(event) {
-    event.preventDefault();
-    copyEmailToClipboard();
-});
+document
+    .getElementById('copyEmailButton')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        copyEmailToClipboard();
+    });
