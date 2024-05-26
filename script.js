0cd687f4d970b4b0424ed156881a673680e0bbde
davidjoves <davidjoves20@gmail.com>
2024-05-26 12:50:00 -0700

    Added alerts and links

---
+++ script.js
@@ -0,0 +1,22 @@
+function copyEmailToClipboard() {
+    const email = "djoves@uci.edu"; // Replace with your email address
+    navigator.clipboard.writeText(email).then(() => {
+        showCustomAlert('Email copied to clipboard!');
+    }).catch(err => {
+        console.error('Could not copy text: ', err);
+    });
+}
+
+function showCustomAlert(message) {
+    const alertBox = document.getElementById('customAlert');
+    alertBox.textContent = message;
+    alertBox.classList.add('show');
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 3000); // The alert will be visible for 3 seconds
+}
+
+document.getElementById('copyEmailButton').addEventListener('click', function(event) {
+    event.preventDefault();
+    copyEmailToClipboard();
+});
